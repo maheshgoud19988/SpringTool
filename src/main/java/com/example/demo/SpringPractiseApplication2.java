9457d318f6b603b30116ea2de3beb7340e55ed1a
Vadla <Vadla@mahesh>
2022-02-24 22:35:45 +0530

    commit test 2 branchX

---
+++ src/main/java/com/example/demo/SpringPractiseApplication2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractiseApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPractiseApplication2.class, args);
+	}
+
+}
